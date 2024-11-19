84e6448831294437d184d100c7ab5fb70451b56f
bkay-stack <kevinbest1900@gmail.com>
2024-11-19 14:11:15 +0100

    Updated item quantity handling

--- src/component/desesert/Dessert.jsx
+++ src/component/desesert/Dessert.jsx
@@ -1,17 +1,19 @@
 import DesseetItem3 from "../dessertlist/DesseetItem3";
 import DessertItem1 from "../dessertlist/DessertItem1";
 import DessertItem2 from "../dessertlist/DessertItem2";
+import Cart from "../cart/Cart";
 import "./dessert.style.css";
 const Dessert = () => {
   return (
     <div className="dessert-card">
       <div className="header">
         <h1>Dessert</h1>
-        <div className="flex">
-          <DessertItem1 />
-          <DessertItem2 />
-          <DesseetItem3 />
-        </div>
+      </div>
+      <div className="flex">
+        <DessertItem1 />
+        <DessertItem2 />
+        <DesseetItem3 />
+        <Cart />
       </div>
     </div>
   );
